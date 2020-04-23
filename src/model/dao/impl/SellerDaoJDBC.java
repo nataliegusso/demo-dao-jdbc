36364d8403b2d22626cf752ba4c1b0acdf27d994
nataliegusso <nataliegusso@gmail.com>
2020-04-23 10:39:26 -0300

    Find implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -51,17 +54,8 @@ public class SellerDaoJDBC implements SellerDao {
 			//os dados estão guardados no BD como tabela, mas preciso ter na memória do comp os objetos associados instanciados em memória (esqueminha java)
 			//a tabela começa em 1, mas a memória do computador começa em 0
 			if (rs.next()) {  //testar se vem resultado, se não vier ele dá falso e pula p return null
-				Department dep = new Department();
-				dep.setId(rs.getInt("DepartmentId"));  //pega do SQL a coluna DepartmentId e coloca no Id da classe department
-				dep.setName(rs.getString("DepName"));
-				
-				Seller obj = new Seller();
-				obj.setId(rs.getInt("Id"));
-				obj.setName(rs.getString("Name"));
-				obj.setEmail(rs.getString("Email"));
-				obj.setBaseSalary(rs.getDouble("BaseSalary"));
-				obj.setBirthDate(rs.getDate("BirthDate"));
-				obj.setDepartment(dep);	//não pega da tabela pq já foi feito acima, pega do obj Department
+				Department dep = instanciateDepartment(rs);
+				Seller obj = instanciateSeller(rs, dep);
 				return obj;
 			}
 			return null;
@@ -75,9 +69,95 @@ public class SellerDaoJDBC implements SellerDao {
 		}
 	}
 
+	private Seller instanciateSeller(ResultSet rs, Department dep) throws SQLException {
+		Seller obj = new Seller();
+		obj.setId(rs.getInt("Id"));
+		obj.setName(rs.getString("Name"));
+		obj.setEmail(rs.getString("Email"));
+		obj.setBaseSalary(rs.getDouble("BaseSalary"));
+		obj.setBirthDate(rs.getDate("BirthDate"));
+		obj.setDepartment(dep);	//não pega da tabela pq já foi feito acima, pega do obj Department
+		return obj;
+	}
+
+	private Department instanciateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("DepartmentId"));  //pega do SQL a coluna DepartmentId e coloca no Id da classe department
+		dep.setName(rs.getString("DepName"));
+		return dep;
+	}
+
 	@Override
-	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Seller> findAll() {  //Cópia do findByDepartment
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "ORDER BY Name");
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();
+			
+			while (rs.next()) {
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				if (dep == null) {
+					dep = instanciateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				Seller obj = instanciateSeller(rs, dep);
+				list.add(obj);
+			}
+			return list;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE DepartmentId = ? "
+					+ "ORDER BY Name");
+			st.setInt(1, department.getId());
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();
+			
+			while (rs.next()) {  //Map: para controlar a não repetição de departamento
+				Department dep = map.get(rs.getInt("DepartmentId"));  //verificar se o departamento já existe, se sim, joga no map (map.get)
+				
+				if (dep == null) {  //se o departamento não existir, instancio um novo
+					dep = instanciateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);  //guardar o novo departamento no map
+				}
+				Seller obj = instanciateSeller(rs, dep);  //instanciar o vendedor apontando p dep
+				list.add(obj);
+			}
+			return list;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally { 	//não fecha conexão pq esse objeto pode ser usado p outra operação. Ideal é fechar no Program
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 }
